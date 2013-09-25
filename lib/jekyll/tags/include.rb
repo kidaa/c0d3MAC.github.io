245c04df29c879e617ab89384f8f77fe39f0e098
c0d3 <c0d3mii@gmail.com>
2013-09-25 03:11:50 +0200

    # Jekyll
    [![Gem Version](https://badge.fury.io/rb/jekyll.png)](http://badge.fury.io/rb/jekyll)
    
    # Jekyll
    
    [![Gem
    Version](https://badge.fury.io/rb/jekyll.png)](http://badge.fury.io/rb/j
    ekyll)
    
    [![Build
    Status](https://secure.travis-ci.org/mojombo/jekyll.png)](https://travis
    -ci.org/mojombo/jekyll)
    [![Code
    Climate](https://codeclimate.com/github/mojombo/jekyll.png)](https://cod
    eclimate.com/github/mojombo/jekyll)
    [![Dependency
    Status](https://gemnasium.com/mojombo/jekyll.png)](https://gemnasium.com
    /mojombo/jekyll)
    [![Coverage
    Status](https://coveralls.io/repos/mojombo/jekyll/badge.png)](https://co
    veralls.io/r/mojombo/jekyll)
    
    By Tom Preston-Werner, Nick Quaranto, and many awesome contributors!
    
    Jekyll is a simple, blog aware, static site generator. It takes a
    template directory (representing the raw form of a website), runs it
    through Textile or Markdown and Liquid converters, and spits out a
    complete, static website suitable for serving with Apache or your
    favorite web server. This is also the engine behind [GitHub
    Pages](http://pages.github.com), which you can use to host your
    project's page or blog right here from GitHub.
    
    ## Getting Started
    
    * [Install](http://jekyllrb.com/docs/installation/) the gem
    * Read up about its [Usage](http://jekyllrb.com/docs/usage/) and
    [Configuration](http://jekyllrb.com/docs/configuration/)
    * Take a gander at some existing
    [Sites](http://wiki.github.com/mojombo/jekyll/sites)
    * Fork and [Contribute](http://jekyllrb.com/docs/contributing/) your
    own modifications
    * Have questions? Check out `#jekyll` on irc.freenode.net.
    
    ## Diving In
    
    * [Migrate](http://jekyllrb.com/docs/migrations/) from your previous
    system
    * Learn how the [YAML Front
    Matter](http://jekyllrb.com/docs/frontmatter/) works
    * Put information on your site with
    [Variables](http://jekyllrb.com/docs/variables/)
    * Customize the [Permalinks](http://jekyllrb.com/docs/permalinks/) your
    posts are generated with
    * Use the built-in [Liquid
    Extensions](http://jekyllrb.com/docs/templates/) to make your life
    easier
    * Use custom [Plugins](http://jekyllrb.com/docs/plugins/) to generate
    content specific to your site
    
    ## Runtime Dependencies
    
    * Commander: Command-line interface constructor (Ruby)
    * Colorator: Colorizes command line output (Ruby)
    * Classifier: Generating related posts (Ruby)
    * Directory Watcher: Auto-regeneration of sites (Ruby)
    * Liquid: Templating system (Ruby)
    * Maruku: Default markdown engine (Ruby)
    * Pygments.rb: Syntax highlighting (Ruby/Python)
    * RedCarpet: Markdown engine (Ruby)
    * Safe YAML: YAML Parser built for security (Ruby)
    
    ## Developer Dependencies
    
    * Kramdown: Markdown-superset converter (Ruby)
    * Launchy: Cross-platform file launcher (Ruby)
    * RDiscount: Discount Markdown Processor (Ruby)
    * RedCloth: Textile support (Ruby)
    * RedGreen: Nicer test output (Ruby)
    * RR: Mocking (Ruby)
    * Shoulda: Test framework (Ruby)
    * SimpleCov: Coverage framework (Ruby)
    
    ## License
    
    See [LICENSE](https://github.com/mojombo/jekyll/blob/master/LICENSE).

---
+++ lib/jekyll/tags/include.rb
@@ -0,0 +1,123 @@
+module Jekyll
+  module Tags
+    class IncludeTag < Liquid::Tag
+
+      SYNTAX_EXAMPLE = "{% include file.ext param='value' param2='value' %}"
+
+      VALID_SYNTAX = /([\w-]+)\s*=\s*(?:"([^"\\]*(?:\\.[^"\\]*)*)"|'([^'\\]*(?:\\.[^'\\]*)*)'|([\w\.-]+))/      
+
+      INCLUDES_DIR = '_includes'
+
+      def initialize(tag_name, markup, tokens)
+        super
+        @file, @params = markup.strip.split(' ', 2);
+        validate_syntax
+      end
+
+      def validate_syntax
+        validate_file_name
+        validate_params if @params
+      end
+
+      def parse_params(context)
+        params = {}
+        markup = @params
+
+        while match = VALID_SYNTAX.match(markup) do
+          markup = markup[match.end(0)..-1]
+
+          value = if match[2]
+            match[2].gsub(/\\"/, '"')
+          elsif match[3]
+            match[3].gsub(/\\'/, "'")
+          elsif match[4]
+            context[match[4]]
+          end
+
+          params[match[1]] = value
+        end
+        params
+      end
+
+      def validate_file_name
+        if @file !~ /^[a-zA-Z0-9_\/\.-]+$/ || @file =~ /\.\// || @file =~ /\/\./
+            raise ArgumentError.new <<-eos
+Invalid syntax for include tag. File contains invalid characters or sequences:
+
+	#{@file}
+
+Valid syntax:
+
+	#{SYNTAX_EXAMPLE}
+
+eos
+        end
+      end
+
+      def validate_params
+        full_valid_syntax = Regexp.compile('\A\s*(?:' + VALID_SYNTAX.to_s + '(?=\s|\z)\s*)*\z')
+        unless @params =~ full_valid_syntax
+          raise ArgumentError.new <<-eos
+Invalid syntax for include tag:
+
+	#{@params}
+
+Valid syntax:
+
+	#{SYNTAX_EXAMPLE}
+
+eos
+        end
+      end
+
+      # Grab file read opts in the context
+      def file_read_opts(context)
+        context.registers[:site].file_read_opts
+      end
+
+      def render(context)
+        dir = File.join(context.registers[:site].source, INCLUDES_DIR)
+        if error = validate_dir(dir, context.registers[:site].safe)
+          return error
+        end
+
+        file = File.join(dir, @file)
+        if error = validate_file(dir, context.registers[:site].safe)
+          return error
+        end
+
+        partial = Liquid::Template.parse(source(file, context))
+
+        context.stack do
+          context['include'] = parse_params(context) if @params
+          partial.render(context)
+        end
+      end
+
+      def validate_dir(dir, safe)
+        if File.symlink?(dir) && safe
+          "Includes directory '#{dir}' cannot be a symlink"
+        end
+      end
+
+      def validate_file(file, safe)
+        if !File.exists?(file)
+          "Included file '#{@file}' not found in '#{INCLUDES_DIR}' directory"
+        elsif File.symlink?(file) && safe
+          "The included file '#{INCLUDES_DIR}/#{@file}' should not be a symlink"
+        end
+      end
+
+      def blank?
+        false
+      end
+
+      # This method allows to modify the file content by inheriting from the class.
+      def source(file, context)
+        File.read_with_options(file, file_read_opts(context))
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('include', Jekyll::Tags::IncludeTag)
