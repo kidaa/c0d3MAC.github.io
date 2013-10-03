7fc8077bd0ee521c2946a4c6a378b4bfd26aae7c
c0d3 <c0d3mii@gmail.com>
2013-10-03 21:24:42 +0200

    @@ -2,4 +2,4 @@ source 'https://rubygems.org'  gem'spec'  gem 'github-pages'  gem "jekyll", "~> 1.2.1" -gem "rake", "~> 0.9.2.2"  +gem "rake", "~> 0.9.2.2"
    @@ -2,4 +2,4 @@ source 'https://rubygems.org'
     gem'spec'
     gem 'github-pages'
     gem "jekyll", "~> 1.2.1"
    -gem "rake", "~> 0.9.2.2" 
    +gem "rake", "~> 0.9.2.2"

---
+++ octopress/rake-0.9.2.2/lib/rake/0.9.2.2.rb
@@ -0,0 +1,7 @@
+require "rake/0.9.2.2/version"
+
+module Rake
+  module 0.9.2.2
+    # Your code goes here...
+  end
+end
---
+++ octopress/rake-0.9.2.2/lib/rake/0.9.2.2/version.rb
@@ -0,0 +1,5 @@
+module Rake
+  module 0.9.2.2
+    VERSION = "0.0.1"
+  end
+end
